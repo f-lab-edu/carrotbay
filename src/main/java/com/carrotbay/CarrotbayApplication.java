645f849df4ff26c76a2575358e8f37cd48b1556a
leemanbokgoo <dmsql5303@gmail.com>
2024-11-29 21:55:06 +0900

    feat : 기본 폴더 생성

---
+++ src/main/java/com/carrotbay/CarrotbayApplication.java
@@ -0,0 +1,13 @@
+package com.carrotbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotbayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrotbay/CarrotbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrotbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
